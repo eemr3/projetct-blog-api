a1bfdfcc60c6adeee36261adc24de3e56c4e5cb1
Emerson <eemr3@yahoo.com.br>
2022-04-10 20:03:45 -0300

    feat: :construction_worker: adiciona rota de get para busca de todos os users e o middleware de autenticação nela

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
+const { authMiddleware } = require('../middlewares/AuthenticateMiddleware');
 
 const routes = express.Router();
 
+routes.get('/', authMiddleware, UserController.getAll);
 routes.post('/', UserController.create);
 
 module.exports = routes;
