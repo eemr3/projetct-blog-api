02675d0f2931faa6821e93a6dfeca9d4e78c62fc
Emerson <eemr3@yahoo.com.br>
2022-04-10 20:32:45 -0300

    feat: :construction_worker: User.routes
    adiciona rota getById  com middleware de validação para acesso ao controller getById

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -5,6 +5,7 @@ const { authMiddleware } = require('../middlewares/AuthenticateMiddleware');
 const routes = express.Router();
 
 routes.get('/', authMiddleware, UserController.getAll);
+routes.get('/:id', authMiddleware, UserController.getById);
 routes.post('/', UserController.create);
 
 module.exports = routes;
