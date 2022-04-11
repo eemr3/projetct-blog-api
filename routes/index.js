721a61d69a409f9b2b105d9cd619c454ff145395
Emerson <eemr3@yahoo.com.br>
2022-04-10 21:20:18 -0300

    feat: :rocket: index.js / routes
    adiciona rota de categorias

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const UserRoutes = require('./User.routes');
 const LoginRoutes = require('./Login.routes');
+const CategoryRoutes = require('./Category.routes');
 
 module.exports = {
   UserRoutes,
   LoginRoutes,
+  CategoryRoutes,
 };
