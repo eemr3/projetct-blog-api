4dc6f6f73772940391ce935876be5b01c5807de1
Emerson <eemr3@yahoo.com.br>
2022-04-10 21:30:07 -0300

    feat: :construction_worker: Category.routes.js
    adiciona rota para getAll

--- routes/Category.routes.js
+++ routes/Category.routes.js
@@ -4,6 +4,7 @@ const { authMiddleware } = require('../middlewares/AuthenticateMiddleware');
 
 const routes = express.Router();
 
+routes.get('/', authMiddleware, CategoryController.getAll);
 routes.post('/', authMiddleware, CategoryController.create);
 
 module.exports = routes;
