c31aeb2e6423009b4ca8f09f92838be55eb386c7
Emerson <eemr3@yahoo.com.br>
2022-04-14 15:29:37 -0300

    refactor: :memo: user.js models
    adiciona associação de tabelas

--- models/user.js
+++ models/user.js
@@ -1,15 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
-    {
+  const User = sequelize.define('User', {
       displayName: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
       image: DataTypes.STRING,
-    },
-    {
-      timestamps: false,
-    },
-  );
+    }, { timestamps: false });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      as: 'BlogPosts',
+      foreignKey: 'userId',
+    });
+  };
   return User;
 };
