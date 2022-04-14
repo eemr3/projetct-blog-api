65ada42d8a15b344b57b932cf450920130ed3772
Emerson <eemr3@yahoo.com.br>
2022-04-14 15:36:36 -0300

    feat: :construction_worker: index.js routes
    adicina a rota de BlogPost

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const UserRoutes = require('./User.routes');
 const LoginRoutes = require('./Login.routes');
 const CategoryRoutes = require('./Category.routes');
+const BlogPostRoutes = require('./BlogPost.routes');
 
 module.exports = {
   UserRoutes,
   LoginRoutes,
   CategoryRoutes,
+  BlogPostRoutes,
 };
