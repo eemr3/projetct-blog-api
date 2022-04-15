849af768d2d51645eef6bc6fdcad86daca288026
Emerson <eemr3@yahoo.com.br>
2022-04-14 21:51:07 -0300

    feat: :construction_worker: BlogPost.routes
    adiciona endpoint getAll

--- routes/BlogPost.routes.js
+++ routes/BlogPost.routes.js
@@ -4,6 +4,7 @@ const { authMiddleware } = require('../middlewares/AuthenticateMiddleware');
 
 const routes = express.Router();
 
+routes.get('/', authMiddleware, BlogPostController.getAll);
 routes.post('/', authMiddleware, BlogPostController.create);
 
 module.exports = routes;
